65e858c1e98714a5d8a97690cf6c07d96e198101
phantekzy <mainilotfi@gmail.com>
2026-03-19 10:59:19 +0100

    Removed the local typedef (it must stay in msh.h) and Added setenv and unsetenv placeholders, as these are mandatory for a real shell

--- src/builtins.c
+++ src/builtins.c
@@ -1,13 +1,5 @@
+#include "../include/builtins.h"
 #include "../include/msh.h"
-
-typedef struct s_builtin {
-  char *name;
-  int (*func)(char **);
-} t_builtin;
-
-int msh_cd(char **args);
-int msh_help(char **args);
-int msh_exit(char **args);
 
 t_builtin g_builtins[] = {
     {"cd", &msh_cd}, {"help", &msh_help}, {"exit", &msh_exit}};
@@ -25,12 +17,22 @@ int msh_cd(char **args) {
     }
   }
 
+  if (strcmp(path, "-") == 0) {
+    path = getenv("OLDPWD");
+    if (!path) {
+      fprintf(stderr, "msh: cd: OLDPWD not set\n");
+      return 1;
+    }
+    printf("%s\n", path);
+  }
+
   if (chdir(path) != 0) {
     perror("msh: cd");
   } else {
     char cwd[1024];
+    char *old_pwd = getenv("PWD");
     if (getcwd(cwd, sizeof(cwd)) != NULL) {
-      setenv("OLDPWD", getenv("PWD") ? getenv("PWD") : "", 1);
+      setenv("OLDPWD", old_pwd ? old_pwd : "", 1);
       setenv("PWD", cwd, 1);
     }
   }
@@ -38,9 +40,9 @@ int msh_cd(char **args) {
 }
 
 int msh_help(char **args) {
-  (void)args; // Avoid unused parameter warning
-  printf("msh - Minimal Shell\n");
-  printf("Type program names and arguments, and hit enter.\n");
+  (void)args;
+  printf("msh - Minimal Shell v1.0\n");
+  printf("Built-in commands:\n");
   for (int i = 0; i < msh_num_builtins(); i++) {
     printf("  - %s\n", g_builtins[i].name);
   }
