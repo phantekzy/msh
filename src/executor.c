7281444b04927cf6dce2867b9882524bb99b0518
phantekzy <mainilotfi@gmail.com>
2026-03-19 10:46:17 +0100

    signal handling for SIGINT (Ctrl+C). A shell should not exit when Ctrl+C is pressed, but the program running inside the shell should.

--- src/executor.c
+++ src/executor.c
@@ -1,21 +1,25 @@
 #include "../include/msh.h"
 
+extern t_builtin g_builtins[];
+int msh_num_builtins(void);
+
 int msh_launch(char **args) {
   pid_t pid;
   int status;
 
   pid = fork();
   if (pid == 0) {
-    // Child process
+    signal(SIGINT, SIG_DFL);
+    signal(SIGQUIT, SIG_DFL);
+
     if (execvp(args[0], args) == -1) {
-      perror("msh");
+      perror("msh: execvp");
     }
     exit(EXIT_FAILURE);
   } else if (pid < 0) {
-    // Error forking
-    perror("msh");
+    perror("msh: fork error");
   } else {
-    // Parent process
+    // Parent process: Wait robustly
     do {
       waitpid(pid, &status, WUNTRACED);
     } while (!WIFEXITED(status) && !WIFSIGNALED(status));
@@ -25,12 +29,12 @@ int msh_launch(char **args) {
 
 int msh_execute(char **args) {
   if (args[0] == NULL) {
-    return 1;
+    return 1; // Empty command entered
   }
 
   for (int i = 0; i < msh_num_builtins(); i++) {
-    if (strcmp(args[0], builtin_str[i]) == 0) {
-      return (*builtin_func[i])(args);
+    if (strcmp(args[0], g_builtins[i].name) == 0) {
+      return g_builtins[i].func(args);
     }
   }
 
