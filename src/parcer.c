16e1102c3582edf95192382db45e57d99862ceb3
phantekzy <mainilotfi@gmail.com>
2026-03-19 10:43:37 +0100

    handling the trailing newline character left by getline and ensuring the dynamic reallocation logic is robust

--- src/parcer.c
+++ src/parcer.c
@@ -1,22 +1,35 @@
 #include "../include/msh.h"
 
+#define MSH_TOK_BUFSIZE 64
+#define MSH_TOK_DELIM " \t\r\n\a"
+
 char *msh_read_line(void) {
   char *line = NULL;
   size_t bufsize = 0;
+  ssize_t len;
 
-  if (getline(&line, &bufsize, stdin) == -1) {
+  len = getline(&line, &bufsize, stdin);
+  if (len == -1) {
     if (feof(stdin)) {
+      printf("exit\n"); // Visual feedback on EOF (Ctrl+D)
       exit(EXIT_SUCCESS);
     } else {
-      perror("msh: getline");
+      perror("msh: getline error");
       exit(EXIT_FAILURE);
     }
   }
+
+  // Strip the trailing newline character
+  if (len > 0 && line[len - 1] == '\n') {
+    line[len - 1] = '\0';
+  }
+
   return line;
 }
 
 char **msh_split_line(char *line) {
-  int bufsize = MSH_TOK_BUFSIZE, position = 0;
+  int bufsize = MSH_TOK_BUFSIZE;
+  int position = 0;
   char **tokens = malloc(bufsize * sizeof(char *));
   char *token;
 
@@ -27,14 +40,13 @@ char **msh_split_line(char *line) {
 
   token = strtok(line, MSH_TOK_DELIM);
   while (token != NULL) {
-    tokens[position] = token;
-    position++;
+    tokens[position++] = token;
 
     if (position >= bufsize) {
       bufsize += MSH_TOK_BUFSIZE;
       tokens = realloc(tokens, bufsize * sizeof(char *));
       if (!tokens) {
-        fprintf(stderr, "msh: allocation error\n");
+        fprintf(stderr, "msh: reallocation error\n");
         exit(EXIT_FAILURE);
       }
     }
