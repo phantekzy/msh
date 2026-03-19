43e7837cdc47b9985294fa5a99ade9e5d939494c
phantekzy <mainilotfi@gmail.com>
2026-03-19 10:45:03 +0100

    Transitioning to an array of structs rather than parallel arrays. This prevents indexing bugs and makes adding new commands cleaner. It also handles /home/phantekzy directory resolution for cd.

--- src/builtins.c
+++ src/builtins.c
@@ -1,25 +1,53 @@
 #include "../include/msh.h"
 
-char *builtin_str[] = {"cd", "help", "exit"};
+typedef struct s_builtin {
+  char *name;
+  int (*func)(char **);
+} t_builtin;
 
-int (*builtin_func[])(char **) = {&msh_cd, &msh_help, &msh_exit};
+int msh_cd(char **args);
+int msh_help(char **args);
+int msh_exit(char **args);
 
-int msh_num_builtins(void) { return sizeof(builtin_str) / sizeof(char *); }
+t_builtin g_builtins[] = {
+    {"cd", &msh_cd}, {"help", &msh_help}, {"exit", &msh_exit}};
+
+int msh_num_builtins(void) { return sizeof(g_builtins) / sizeof(t_builtin); }
 
 int msh_cd(char **args) {
-  if (args[1] == NULL) {
-    fprintf(stderr, "msh: expected argument to \"cd\"\n");
+  char *path = args[1];
+
+  if (path == NULL) {
+    path = getenv("HOME");
+    if (path == NULL) {
+      fprintf(stderr, "msh: cd: HOME not set\n");
+      return 1;
+    }
+  }
+
+  if (chdir(path) != 0) {
+    perror("msh: cd");
   } else {
-    if (chdir(args[1]) != 0) {
-      perror("msh");
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+      setenv("OLDPWD", getenv("PWD") ? getenv("PWD") : "", 1);
+      setenv("PWD", cwd, 1);
     }
   }
   return 1;
 }
 
 int msh_help(char **args) {
-  printf("msh: My Shell\nBuilt-in commands: cd, help, exit\n");
+  (void)args; // Avoid unused parameter warning
+  printf("msh - Minimal Shell\n");
+  printf("Type program names and arguments, and hit enter.\n");
+  for (int i = 0; i < msh_num_builtins(); i++) {
+    printf("  - %s\n", g_builtins[i].name);
+  }
   return 1;
 }
 
-int msh_exit(char **args) { return 0; }
+int msh_exit(char **args) {
+  (void)args;
+  return 0;
+}
