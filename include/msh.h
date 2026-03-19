2646fbd1fa1dec9e0278394a5ce7cc88f09308b4
phantekzy <mainilotfi@gmail.com>
2026-03-19 10:55:18 +0100

    fixing headers main

--- include/msh.h
+++ include/msh.h
@@ -1,7 +1,7 @@
 #ifndef MSH_H
 #define MSH_H
 
-/* System Libraries  */
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,23 +9,12 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-/* Constants */
 #define MSH_TOK_BUFSIZE 64
 #define MSH_TOK_DELIM " \t\r\n\a"
 
-/* Function Prototypes -   */
-char *msh_read_line(void);
-char **msh_split_line(char *line);
-int msh_execute(char **args);
-int msh_launch(char **args);
-int msh_num_builtins(void);
-
-/* Built-in Commands */
-int msh_cd(char **args);
-int msh_help(char **args);
-int msh_exit(char **args);
-
-extern char *builtin_str[];
-extern int (*builtin_func[])(char **);
+typedef struct s_builtin {
+  char *name;
+  int (*func)(char **);
+} t_builtin;
 
 #endif
